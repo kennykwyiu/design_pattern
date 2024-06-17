0aaa08ed80670574eb152083203db42c7fdbac08
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-17 21:43:55 +0800

    created ICourse interface

---
+++ src/main/java/org/kenny/design/principle/App.java
@@ -0,0 +1,13 @@
+package org.kenny.design.principle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
