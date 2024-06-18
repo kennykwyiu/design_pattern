be375717c49fe589de5086568aceb01be6b37811
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-18 21:40:53 +0800

    interface segregation v1

---
+++ src/main/java/org/kenny/design/principle/interfacesegregation/Bird.java
@@ -0,0 +1,18 @@
+package org.kenny.design.principle.interfacesegregation;
+
+public class Bird implements IAnimalAction{
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+
+    @Override
+    public void swim() {
+
+    }
+}
---
+++ src/main/java/org/kenny/design/principle/interfacesegregation/IEatAnimalAction.java
@@ -0,0 +1,5 @@
+package org.kenny.design.principle.interfacesegregation;
+
+public interface IEatAnimalAction {
+    void eat();
+}
