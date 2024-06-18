5341ef775f4196db62b21e3aa7b5109289b54844
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-18 21:52:30 +0800

    demeter v2 improved

--- src/main/java/org/kenny/design/principle/demeter/Boss.java
+++ src/main/java/org/kenny/design/principle/demeter/Boss.java
@@ -5,10 +5,7 @@ import java.util.List;
 
 public class Boss {
     public void commandCheckNumber(TeamLeader teamLeader) {
-        List<Course> courseList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            courseList.add(new Course());
-        }
-        teamLeader.checkNumberOfCourses(courseList);
+
+        teamLeader.checkNumberOfCourses();
     }
 }
--- src/main/java/org/kenny/design/principle/demeter/TeamLeader.java
+++ src/main/java/org/kenny/design/principle/demeter/TeamLeader.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TeamLeader {
-    public void checkNumberOfCourses(List<Course> courseList) {
+    public void checkNumberOfCourses() {
+        List<Course> courseList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            courseList.add(new Course());
+        }
         System.out.println("the qty of online courses are " + courseList.size());
     }
 
