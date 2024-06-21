cce2ba07df9cc96dfae028468edc190ecf4ab0dd
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-21 20:51:38 +0800

    simple factory - wrong example

---
+++ src/main/java/org/kenny/design/pattern/creational/simplefactory/PythonVideo.java
@@ -0,0 +1,8 @@
+package org.kenny.design.pattern.creational.simplefactory;
+
+public class PythonVideo extends Video{
+    @Override
+    public void produce() {
+        System.out.println("Recording Python lesson classes");
+    }
+}
