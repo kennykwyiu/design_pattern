c38cce7c2663e430bcd300996111449444fa6305
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-22 10:25:23 +0800

    abstract factory - test

--- src/main/java/org/kenny/design/pattern/creational/abstractfactory/JavaArticle.java
+++ src/main/java/org/kenny/design/pattern/creational/abstractfactory/JavaArticle.java
@@ -3,6 +3,6 @@ package org.kenny.design.pattern.creational.abstractfactory;
 public class JavaArticle extends Article {
     @Override
     public void produce() {
-        System.out.println("Create Java article");
+        System.out.println("Creating Java article");
     }
 }
--- src/main/java/org/kenny/design/pattern/creational/abstractfactory/PythonArticle.java
+++ src/main/java/org/kenny/design/pattern/creational/abstractfactory/PythonArticle.java
@@ -3,6 +3,6 @@ package org.kenny.design.pattern.creational.abstractfactory;
 public class PythonArticle extends Article {
     @Override
     public void produce() {
-        System.out.println("Create Python article");
+        System.out.println("Creating Python article");
     }
 }
