beef8fd558b5a1010c772baa74057c6bc4f41fe8
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-23 16:17:49 +0800

    builder pattern - start

---
+++ src/main/java/org/kenny/design/pattern/creational/builder/Course.java
@@ -0,0 +1,60 @@
+package org.kenny.design.pattern.creational.builder;
+
+public class Course {
+    private String courseName;
+    private String coursePpt;
+    private String courseVideo;
+    private String courseArticle;
+    private String courseQA;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCoursePpt() {
+        return coursePpt;
+    }
+
+    public void setCoursePpt(String coursePpt) {
+        this.coursePpt = coursePpt;
+    }
+
+    public String getCourseVideo() {
+        return courseVideo;
+    }
+
+    public void setCourseVideo(String courseVideo) {
+        this.courseVideo = courseVideo;
+    }
+
+    public String getCourseArticle() {
+        return courseArticle;
+    }
+
+    public void setCourseArticle(String courseArticle) {
+        this.courseArticle = courseArticle;
+    }
+
+    public String getCourseQA() {
+        return courseQA;
+    }
+
+    public void setCourseQA(String courseQA) {
+        this.courseQA = courseQA;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "courseName='" + courseName + '\'' +
+                ", coursePpt='" + coursePpt + '\'' +
+                ", courseVideo='" + courseVideo + '\'' +
+                ", courseArticle='" + courseArticle + '\'' +
+                ", courseQA='" + courseQA + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/kenny/design/pattern/creational/builder/CourseBuilder.java
@@ -0,0 +1,9 @@
+package org.kenny.design.pattern.creational.builder;
+
+public abstract class CourseBuilder {
+    private String courseName;
+    private String coursePpt;
+    private String courseVideo;
+    private String courseArticle;
+    private String courseQA;
+}
