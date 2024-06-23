afc167d58a1dd29cc9ce8ec990c030f51703e235
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-23 16:20:18 +0800

    builder pattern - abstract CourseBuilder

--- src/main/java/org/kenny/design/pattern/creational/builder/CourseBuilder.java
+++ src/main/java/org/kenny/design/pattern/creational/builder/CourseBuilder.java
@@ -1,9 +1,12 @@
 package org.kenny.design.pattern.creational.builder;
 
 public abstract class CourseBuilder {
-    private String courseName;
-    private String coursePpt;
-    private String courseVideo;
-    private String courseArticle;
-    private String courseQA;
+
+    public abstract void buildCourseName(String courseName);
+    public abstract void buildCoursePpt(String coursePpt);
+    public abstract void buildCourseVideo(String courseVideo);
+    public abstract void buildCourseArticle(String courseArticle);
+    public abstract void buildCourseQA(String courseQA);
+
+    public abstract Course makeCourse();
 }
