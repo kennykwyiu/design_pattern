77f6dcf389ca01402c2b9e56274c25e0012ccc1f
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-24 20:22:24 +0800

    builder pattern - v2 - finished

--- src/main/java/org/kenny/design/pattern/creational/builder/v2/Course.java
+++ src/main/java/org/kenny/design/pattern/creational/builder/v2/Course.java
@@ -10,6 +10,14 @@ public class Course {
     private String courseArticle;
     private String courseQA;
 
+    public Course(CourseBuilder courseBuilder) {
+        this.courseName = courseBuilder.courseName;
+        this.coursePpt = courseBuilder.coursePpt;
+        this.courseVideo = courseBuilder.courseVideo;
+        this.courseArticle = courseBuilder.courseArticle;
+        this.courseQA = courseBuilder.courseQA;
+    }
+
     @Override
     public String toString() {
         return "Course{" +
@@ -21,5 +29,40 @@ public class Course {
                 '}';
     }
 
+    public static class CourseBuilder {
+        private String courseName;
+        private String coursePpt;
+        private String courseVideo;
+        private String courseArticle;
+        private String courseQA;
 
+        public CourseBuilder buildCourseName(String courseArticle) {
+            this.courseName = courseName;
+            return this;
+        }
+
+        public CourseBuilder buildCoursePpt(String coursePpt) {
+            this.coursePpt = coursePpt;
+            return this;
+        }
+
+        public CourseBuilder buildCourseVideo(String courseVideo) {
+            this.courseVideo = courseVideo;
+            return this;
+        }
+
+        public CourseBuilder buildCourseArticle(String courseArticle) {
+            this.courseArticle = courseArticle;
+            return this;
+        }
+
+        public CourseBuilder buildCourseQA(String courseQA) {
+            this.courseQA = courseQA;
+            return this;
+        }
+
+        public Course build() {
+            return new Course(this);
+        }
+    }
 }
