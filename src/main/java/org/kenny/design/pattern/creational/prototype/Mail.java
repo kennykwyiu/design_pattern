afe2300b0e2d8cd15ab77b21d27cad5f2c1f7a02
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-28 21:13:33 +0800

    prototype - improved Mail - implements Cloneable

--- src/main/java/org/kenny/design/pattern/creational/prototype/Mail.java
+++ src/main/java/org/kenny/design/pattern/creational/prototype/Mail.java
@@ -1,11 +1,11 @@
 package org.kenny.design.pattern.creational.prototype;
 
-public class Mail {
+public class Mail implements Cloneable {
     private String name;
     private String emailAddress;
     private String content;
 
-    public  Mail() {
+    public Mail() {
         System.out.println("Mail Class Constructor");
     }
 
@@ -39,6 +39,12 @@ public class Mail {
                 "name='" + name + '\'' +
                 ", emailAddress='" + emailAddress + '\'' +
                 ", content='" + content + '\'' +
-                '}';
+                '}' + super.toString();
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        System.out.println("clone mail object");
+        return super.clone();
     }
 }
--- src/main/java/org/kenny/design/pattern/creational/prototype/Test.java
+++ src/main/java/org/kenny/design/pattern/creational/prototype/Test.java
@@ -1,16 +1,20 @@
 package org.kenny.design.pattern.creational.prototype;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         Mail mail = new Mail();
         mail.setContent("initial template");
+        System.out.println("initial mail: " + mail);
 
         for (int i = 0; i < 10; i++) {
-            mail.setName("name" + i);
-            mail.setEmailAddress("name"+i+"@gmail.com");
-            mail.setContent("Congratulation! You got a gift from Google");
-            MailUtil.sendMail(mail);
+            Mail mailTemp = (Mail) mail.clone();
+            mailTemp.setName("name" + i);
+            mailTemp.setEmailAddress("name"+i+"@gmail.com");
+            mailTemp.setContent("Congratulation! You got a gift from Google");
+            MailUtil.sendMail(mailTemp);
+            System.out.println("cloned mailTemp: " + mailTemp);
         }
         MailUtil.saveOriginMailRecord(mail);
+        System.out.println(mail);
     }
 }
