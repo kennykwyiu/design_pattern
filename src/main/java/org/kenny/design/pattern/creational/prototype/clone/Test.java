c371226e3ff8d2f108b02a056e0da9e8e7389bc3
kennykwyiu <kenny.kwyiu@gmail.com>
2024-06-30 23:10:32 +0800

    abstract prototype

--- src/main/java/org/kenny/design/pattern/creational/prototype/clone/Test.java
+++ src/main/java/org/kenny/design/pattern/creational/prototype/clone/Test.java
@@ -8,16 +8,16 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) throws CloneNotSupportedException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-//        Date birthday = new Date(0L);
-//        Pig pig1 = new Pig("Peggy", birthday);
-//        Pig pig2 = (Pig) pig1.clone();
-//
-//        System.out.println(pig1);
-//        System.out.println(pig2);
-//
-//        pig1.getBirthday().setTime(66666666666L);
-//        System.out.println(pig1);
-//        System.out.println(pig2);
+        Date birthday = new Date(0L);
+        Pig pig1 = new Pig("Peggy", birthday);
+        Pig pig2 = (Pig) pig1.clone();
+
+        System.out.println(pig1);
+        System.out.println(pig2);
+
+        pig1.getBirthday().setTime(66666666666L);
+        System.out.println(pig1);
+        System.out.println(pig2);
 
         HungrySingleton hungrySingleton = HungrySingleton.getInstance();
         Method method = hungrySingleton.getClass().getDeclaredMethod("clone");
