b29da3a0e82ae8e8fd361bda89cefb8421937ae9
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-01 19:15:14 +0800

    facade - added GiftExchangeService

---
+++ src/main/java/org/kenny/design/pattern/structural/facade/GiftExchangeService.java
@@ -0,0 +1,28 @@
+package org.kenny.design.pattern.structural.facade;
+
+public class GiftExchangeService {
+    private QualifyService qualifyService;
+    private PointsPaymentService pointsPaymentService;
+    private ShippingService shippingService;
+
+    public void setQualifyService(QualifyService qualifyService) {
+        this.qualifyService = qualifyService;
+    }
+
+    public void setPointsPaymentService(PointsPaymentService pointsPaymentService) {
+        this.pointsPaymentService = pointsPaymentService;
+    }
+
+    public void setShippingService(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
+    public void giftExchange(PointsGift pointsGift) {
+        if (qualifyService.isAvailable(pointsGift)) {
+            if (pointsPaymentService.pay(pointsGift)) {
+                String shippingOrderNo = shippingService.shipGift(pointsGift);
+                System.out.println("logistic system was ordered successfully, shipping order no. is " + shippingOrderNo);
+            }
+        }
+    }
+}
