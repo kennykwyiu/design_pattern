6e9b69deb53ea93b5f1f16e1e2de16ccfcbd870c
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-03 20:25:17 +0800

    adapter - Adaptee and Target

---
+++ src/main/java/org/kenny/design/pattern/structural/adapter/classadapter/Adaptee.java
@@ -0,0 +1,7 @@
+package org.kenny.design.pattern.structural.adapter.classadapter;
+
+public class Adaptee {
+    public void AdapteeRequest() {
+        System.out.println("The method from the Adaptee class");
+    }
+}
---
+++ src/main/java/org/kenny/design/pattern/structural/adapter/classadapter/Target.java
@@ -0,0 +1,5 @@
+package org.kenny.design.pattern.structural.adapter.classadapter;
+
+public interface Target {
+    void request();
+}
