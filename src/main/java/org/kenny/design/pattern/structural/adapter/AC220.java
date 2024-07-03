dfaebb05d866cda2b5568fa4b5b28b4d06d5fbda
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-03 20:40:59 +0800

    adapter - real example - AC220 (adaptee)

---
+++ src/main/java/org/kenny/design/pattern/structural/adapter/AC220.java
@@ -0,0 +1,9 @@
+package org.kenny.design.pattern.structural.adapter;
+
+public class AC220 {
+    public int outputAC220V() {
+        int output = 220;
+        System.out.println("output exchange electricity " + output + "V");
+        return output;
+    }
+}
