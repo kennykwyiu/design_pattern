e24837cd587541d334231d07d2cea5158c2cd29a
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-04 16:03:26 +0800

    flyweight - created EmployeeFactory

---
+++ src/main/java/org/kenny/design/pattern/structural/flyweight/EmployeeFactory.java
@@ -0,0 +1,19 @@
+package org.kenny.design.pattern.structural.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeFactory {
+    private static final Map<String, Employee> EMPLOYEE_MAP = new HashMap<>();
+
+    public static Employee getManager(String department) {
+        Manager manager = (Manager) EMPLOYEE_MAP.get(department);
+
+        if (manager == null) {
+            manager = new Manager(department);
+            EMPLOYEE_MAP.put(department, manager);
+            System.out.println("created department manager: " + department);
+        }
+        return manager;
+    }
+}
