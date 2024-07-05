a54403eea75cc2f368abd79b12157ce4f1c67cd4
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-05 21:47:22 +0800

    composite - created Course

---
+++ src/main/java/org/kenny/design/pattern/structural/composite/Course.java
@@ -0,0 +1,26 @@
+package org.kenny.design.pattern.structural.composite;
+
+public class Course extends CatalogComponent {
+    private String name;
+    private double price;
+
+    public Course(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String getName(CatalogComponent catalogComponent) {
+        return this.name;
+    }
+
+    @Override
+    public double getPrice(CatalogComponent catalogComponent) {
+        return this.price;
+    }
+
+    @Override
+    public void print(CatalogComponent catalogComponent) {
+        System.out.println("Course Name: " + name + " Price: " + price);
+    }
+}
