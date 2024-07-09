1dcc631eebc4dfc3bd227373177b370804847511
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-09 22:12:11 +0800

    proxy - created interface of IOrderService and IOrderDao

---
+++ src/main/java/org/kenny/design/pattern/structural/proxy/IOrderDao.java
@@ -0,0 +1,5 @@
+package org.kenny.design.pattern.structural.proxy;
+
+public interface IOrderDao {
+    int insert(Order order);
+}
---
+++ src/main/java/org/kenny/design/pattern/structural/proxy/IOrderService.java
@@ -0,0 +1,5 @@
+package org.kenny.design.pattern.structural.proxy;
+
+public interface IOrderService {
+    int saveOrder(Order order);
+}
