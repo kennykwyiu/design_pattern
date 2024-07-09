81ef4442c469d1353f7922e1882850ce411f6d9b
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-09 22:33:15 +0800

    proxy - created DataSourceContextHolder

---
+++ src/main/java/org/kenny/design/pattern/structural/proxy/db/DataSourceContextHolder.java
@@ -0,0 +1,16 @@
+package org.kenny.design.pattern.structural.proxy.db;
+
+public class DataSourceContextHolder {
+    private static final ThreadLocal<String> CONTEXT_HOLDER = new ThreadLocal<>();
+
+    public static void setDBType(String dbType) {
+        CONTEXT_HOLDER.set(dbType);
+    }
+    public static String getDBType() {
+        return (String) CONTEXT_HOLDER.get();
+    }
+
+    public static void clearDBType() {
+        CONTEXT_HOLDER.remove();
+    }
+}
