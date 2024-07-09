5dcd5f263786a8d1f9a38b8dc1a56415e11414aa
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-09 22:34:19 +0800

    proxy - created DynamicDataSource

---
+++ src/main/java/org/kenny/design/pattern/structural/proxy/db/DynamicDataSource.java
@@ -0,0 +1,10 @@
+package org.kenny.design.pattern.structural.proxy.db;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return DataSourceContextHolder.getDBType();
+    }
+}
