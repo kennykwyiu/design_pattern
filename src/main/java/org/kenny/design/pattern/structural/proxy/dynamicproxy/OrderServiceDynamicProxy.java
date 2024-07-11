46b6cd3853c5b517823afbf0d8749a31f40c19fd
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-11 21:49:19 +0800

    proxy - created OrderServiceDynamicProxy

---
+++ src/main/java/org/kenny/design/pattern/structural/proxy/dynamicproxy/OrderServiceDynamicProxy.java
@@ -0,0 +1,24 @@
+package org.kenny.design.pattern.structural.proxy.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+public class OrderServiceDynamicProxy implements InvocationHandler {
+
+    private Object target;
+
+    public OrderServiceDynamicProxy(Object target) {
+        this.target = target;
+    }
+
+    public Object bind() {
+        Class cls = target.getClass();
+        return Proxy.newProxyInstance(cls.getClassLoader(), cls.getInterfaces(), this);
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        return null;
+    }
+}
