76a5db90463ceb159a9daa36dd6404f730ce2cc1
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-11 21:57:30 +0800

    proxy -  OrderServiceDynamicProxy - implemented beforeMethod and afterMethod

--- src/main/java/org/kenny/design/pattern/structural/proxy/dynamicproxy/OrderServiceDynamicProxy.java
+++ src/main/java/org/kenny/design/pattern/structural/proxy/dynamicproxy/OrderServiceDynamicProxy.java
@@ -1,5 +1,8 @@
 package org.kenny.design.pattern.structural.proxy.dynamicproxy;
 
+import org.kenny.design.pattern.structural.proxy.Order;
+import org.kenny.design.pattern.structural.proxy.db.DataSourceContextHolder;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -20,5 +23,22 @@ public class OrderServiceDynamicProxy implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         return null;
+    }
+
+    private void beforeMethod(Object object) {
+        int userId = 0;
+        System.out.println("Dynamic proxy before code");
+        if (object instanceof Order) {
+            userId = ((Order) object).getUserId();
+        }
+        int dbRouter = userId % 2;
+        System.out.println("Dynamic proxy arrange [db" + dbRouter + "] to handle the data");
+
+        //todo setting dataSource;
+        DataSourceContextHolder.setDBType("db" + String.valueOf(dbRouter));
+    }
+
+    private void afterMethod() {
+        System.out.println("Dynamic proxy after code");
     }
 }
