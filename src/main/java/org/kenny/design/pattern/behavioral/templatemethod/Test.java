259e28856ea0c3b336a60dfccb725d37a68ddd3d
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-12 21:10:23 +0800

    template method - created Test

---
+++ src/main/java/org/kenny/design/pattern/behavioral/templatemethod/Test.java
@@ -0,0 +1,15 @@
+package org.kenny.design.pattern.behavioral.templatemethod;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("--- backend design pattern course - [start] ---");
+        ACourse designPatternCourse = new DesignPatternCourse();
+        designPatternCourse.makeCourse();
+        System.out.println("--- backend design pattern course - [end] ---");
+
+        System.out.println("--- front-end web development course - [start] ---");
+        ACourse webDevelopmentCourse = new WebDevelopmentCourse();
+        webDevelopmentCourse.makeCourse();
+        System.out.println("--- front-end web development course - [end] ---");
+    }
+}
