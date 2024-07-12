bc6228bb32228b07d2d5d418255d90018df3832c
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-12 21:10:08 +0800

    template method - override needWriteArticle in DesignPatternCourse

--- src/main/java/org/kenny/design/pattern/behavioral/templatemethod/DesignPatternCourse.java
+++ src/main/java/org/kenny/design/pattern/behavioral/templatemethod/DesignPatternCourse.java
@@ -3,6 +3,11 @@ package org.kenny.design.pattern.behavioral.templatemethod;
 public class DesignPatternCourse extends ACourse{
     @Override
     void packageCourse() {
-        System.out.println("provdie the source code of Java course");
+        System.out.println("provide the source code of Java course");
+    }
+
+    @Override
+    protected boolean needWriteArticle() {
+        return true;
     }
 }
