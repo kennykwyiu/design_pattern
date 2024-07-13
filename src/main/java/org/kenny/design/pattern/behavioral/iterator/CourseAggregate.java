afd8058ca2fc65b08038aefd7255b06950fbbfa6
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-13 20:25:39 +0800

    iterator - CourseAggregate

---
+++ src/main/java/org/kenny/design/pattern/behavioral/iterator/CourseAggregate.java
@@ -0,0 +1,8 @@
+package org.kenny.design.pattern.behavioral.iterator;
+
+public interface CourseAggregate {
+    void addCourse(Course course);
+    void removeCourse(Course course);
+
+    CourseIterator getCourseIterator();
+}
