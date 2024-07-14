cc21ddcf7f3ba05f52a50bee51fe880f6fbc6264
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-14 15:19:26 +0800

    strategy - here will be lots of if else logic

--- src/main/java/org/kenny/design/pattern/behavioral/strategy/Test.java
+++ src/main/java/org/kenny/design/pattern/behavioral/strategy/Test.java
@@ -1,11 +1,29 @@
 package org.kenny.design.pattern.behavioral.strategy;
 
-public class Test {
-    public static void main(String[] args) {
-        PromotionActivity promotionActivity618 = new PromotionActivity(new BuyOneGetOneFreePromotionStrategy());
-        PromotionActivity promotionActivity1111 = new PromotionActivity(new CashRebatePromotionStrategy());
+import org.apache.commons.lang3.StringUtils;
 
-        promotionActivity618.executePromotionStrategy();
-        promotionActivity1111.executePromotionStrategy();
+public class Test {
+//    public static void main(String[] args) {
+//        PromotionActivity promotionActivity618 = new PromotionActivity(new BuyOneGetOneFreePromotionStrategy());
+//        PromotionActivity promotionActivity1111 = new PromotionActivity(new CashRebatePromotionStrategy());
+//
+//        promotionActivity618.executePromotionStrategy();
+//        promotionActivity1111.executePromotionStrategy();
+//    }
+
+    public static void main(String[] args) {
+        PromotionActivity promotionActivity = null;
+        String promotionKey = "CashCoupon";
+
+        if (StringUtils.equals(promotionKey, "CashCoupon")) {
+            promotionActivity = new PromotionActivity(new CashCouponPromotionStrategy());
+        } else if (StringUtils.equals(promotionKey, "CashRebate")) {
+            promotionActivity = new PromotionActivity(new CashRebatePromotionStrategy());
+        } else if (StringUtils.equals(promotionKey, "BuyOneGetOneFree")) {
+            promotionActivity = new PromotionActivity(new BuyOneGetOneFreePromotionStrategy());
+        } //....
+
+        promotionActivity.executePromotionStrategy();
     }
+
 }
