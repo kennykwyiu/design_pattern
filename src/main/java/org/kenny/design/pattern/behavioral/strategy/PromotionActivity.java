b7c60d3bb218c6b8e03c54f78992e36c22dca5d8
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-14 15:06:24 +0800

    strategy - created PromotionActivity

---
+++ src/main/java/org/kenny/design/pattern/behavioral/strategy/PromotionActivity.java
@@ -0,0 +1,14 @@
+package org.kenny.design.pattern.behavioral.strategy;
+
+public class PromotionActivity {
+    private PromotionStrategy promotionStrategy;
+
+    public PromotionActivity(PromotionStrategy promotionStrategy) {
+        this.promotionStrategy = promotionStrategy;
+    }
+
+
+    public void executePromotionStrategy() {
+        promotionStrategy.doPromotion();
+    }
+}
