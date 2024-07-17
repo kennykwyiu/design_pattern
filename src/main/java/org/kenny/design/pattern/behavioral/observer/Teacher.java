49658e893ea12dd91987964656831ce3ae2c4b9a
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-17 22:27:10 +0800

    observer - created Teacher

---
+++ src/main/java/org/kenny/design/pattern/behavioral/observer/Teacher.java
@@ -0,0 +1,11 @@
+package org.kenny.design.pattern.behavioral.observer;
+
+public class Teacher {
+    private String teacherName;
+
+    public Teacher(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+
+}
