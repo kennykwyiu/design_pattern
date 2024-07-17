46ec72ba851e704fec7d201eda51dd9471b6491b
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-17 22:23:49 +0800

    observer - created Course

---
+++ src/main/java/org/kenny/design/pattern/behavioral/observer/Course.java
@@ -0,0 +1,13 @@
+package org.kenny.design.pattern.behavioral.observer;
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
