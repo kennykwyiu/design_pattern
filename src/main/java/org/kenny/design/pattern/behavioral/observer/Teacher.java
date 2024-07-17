ec15bec729b4d5e19e5cef58a9154681f33e663c
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-17 22:35:57 +0800

    observer - Teacher implements Observer

--- src/main/java/org/kenny/design/pattern/behavioral/observer/Teacher.java
+++ src/main/java/org/kenny/design/pattern/behavioral/observer/Teacher.java
@@ -1,6 +1,9 @@
 package org.kenny.design.pattern.behavioral.observer;
 
-public class Teacher {
+import java.util.Observable;
+import java.util.Observer;
+
+public class Teacher implements Observer {
     private String teacherName;
 
     public Teacher(String teacherName) {
@@ -8,4 +11,10 @@ public class Teacher {
     }
 
 
+    @Override
+    public void update(Observable o, Object arg) {
+        Course course = (Course) o;
+        Question question = (Question) arg;
+        System.out.println(teacherName + " teacher's" + course.getCourseName() + " got a question from " + question.getUserName() + "! Question: " + question.getQuestionContent());
+    }
 }
