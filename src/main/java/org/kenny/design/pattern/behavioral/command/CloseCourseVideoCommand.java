83a5078a2dac2d32d6a2b46850a78471a3eba27f
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-19 22:43:51 +0800

    command - implemented Command

---
+++ src/main/java/org/kenny/design/pattern/behavioral/command/CloseCourseVideoCommand.java
@@ -0,0 +1,14 @@
+package org.kenny.design.pattern.behavioral.command;
+
+public class CloseCourseVideoCommand implements Command {
+    private CourseVideo courseVideo;
+
+    public CloseCourseVideoCommand(CourseVideo courseVideo) {
+        this.courseVideo = courseVideo;
+    }
+
+    @Override
+    public void execute() {
+        courseVideo.close();
+    }
+}
---
+++ src/main/java/org/kenny/design/pattern/behavioral/command/OpenCourseVideoCommand.java
@@ -0,0 +1,14 @@
+package org.kenny.design.pattern.behavioral.command;
+
+public class OpenCourseVideoCommand implements Command {
+    private CourseVideo courseVideo;
+
+    public OpenCourseVideoCommand(CourseVideo courseVideo) {
+        this.courseVideo = courseVideo;
+    }
+
+    @Override
+    public void execute() {
+        courseVideo.open();
+    }
+}
