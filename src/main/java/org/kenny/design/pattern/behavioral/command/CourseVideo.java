5b9934eaf35c0e6184fc8560708cac7e2f9d98ba
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-19 22:37:49 +0800

    command - created CourseVideo

---
+++ src/main/java/org/kenny/design/pattern/behavioral/command/CourseVideo.java
@@ -0,0 +1,16 @@
+package org.kenny.design.pattern.behavioral.command;
+
+public class CourseVideo {
+    private String name;
+    public CourseVideo(String name) {
+        this.name = name;
+    }
+
+    public void open() {
+        System.out.println(this.name + " course video is opened for all people");
+    }
+
+    public void close() {
+        System.out.println(this.name + "  course video is closed");
+    }
+}
