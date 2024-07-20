6439d85efad2a2c1bfe13e373a176fa22a09041a
kennykwyiu <kenny.kwyiu@gmail.com>
2024-07-20 13:50:57 +0800

    memento - created ArticleMementoManager

---
+++ src/main/java/org/kenny/design/pattern/behavioral/memento/ArticleMementoManager.java
@@ -0,0 +1,16 @@
+package org.kenny.design.pattern.behavioral.memento;
+
+import java.util.Stack;
+
+public class ArticleMementoManager {
+    private final Stack<ArticleMemento> ARTICLE_MEMENTO_STACK = new Stack<>();
+
+    public ArticleMemento getMemento() {
+        ArticleMemento articleMemento = ARTICLE_MEMENTO_STACK.pop();
+        return articleMemento;
+    }
+
+    public void addMemento(ArticleMemento articleMemento) {
+        ARTICLE_MEMENTO_STACK.push(articleMemento);
+    }
+}
