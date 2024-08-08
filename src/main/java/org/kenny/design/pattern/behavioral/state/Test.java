cee7160249dd9607e5835de7580e8436de500c0b
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-08 21:14:09 +0800

    state - created Test

---
+++ src/main/java/org/kenny/design/pattern/behavioral/state/Test.java
@@ -0,0 +1,17 @@
+package org.kenny.design.pattern.behavioral.state;
+
+public class Test {
+    public static void main(String[] args) {
+        CourseVideoContext courseVideoContext = new CourseVideoContext();
+        courseVideoContext.setCourseVideoState(new PlayState());
+
+        System.out.println("current state: " + courseVideoContext.getCourseVideoState().getClass().getSimpleName());
+        courseVideoContext.pause();
+        System.out.println("current state: " + courseVideoContext.getCourseVideoState().getClass().getSimpleName());
+        courseVideoContext.speed();
+        System.out.println("current state: " + courseVideoContext.getCourseVideoState().getClass().getSimpleName());
+        courseVideoContext.stop();
+        System.out.println("current state: " + courseVideoContext.getCourseVideoState().getClass().getSimpleName());
+        courseVideoContext.speed();  
+    }
+}
