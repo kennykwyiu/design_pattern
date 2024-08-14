bfaf1357864ea4aba11b2cc393a2b633d925b55a
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-14 20:36:29 +0800

    abstract factory - cryptographic_library example - concrete class

---
+++ src/main/java/org/kenny/design/pattern/creational/abstractfactory/cryptographic_library/SSLSecureProtocol.java
@@ -0,0 +1,9 @@
+package org.kenny.design.pattern.creational.abstractfactory.cryptographic_library;
+
+public class SSLSecureProtocol implements SecureProtocol {
+    @Override
+    public void establishSecureConnection() {
+
+    }
+    // Implement SSL secure connection establishment
+}
---
+++ src/main/java/org/kenny/design/pattern/creational/abstractfactory/cryptographic_library/TLSSecureProtocol.java
@@ -0,0 +1,9 @@
+package org.kenny.design.pattern.creational.abstractfactory.cryptographic_library;
+
+public class TLSSecureProtocol implements SecureProtocol {
+    @Override
+    public void establishSecureConnection() {
+
+    }
+    // Implement TLS secure connection establishment
+}
