37c04b49be369e59c44475ea618941d3b4477215
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-22 23:26:21 +0800

    composite - financial_application example

--- src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialInstrument.java
+++ src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialInstrument.java
@@ -2,14 +2,23 @@ package org.kenny.design.pattern.structural.composite.financial_application;
 
 public class FinancialInstrument implements FinancialElement {
     private String name;
+    private double value;
 
-    public FinancialInstrument(String name) {
+    public FinancialInstrument(String name, double value) {
         this.name = name;
+        this.value = value;
     }
 
     @Override
     public void display() {
-        System.out.println("Financial Instrument: " + name);
+        System.out.println("Financial Instrument: " + name + " - Value: $" + value);
     }
+
+    @Override
+    public double calculateValue() {
+        return value;
+    }
+
+
 }
 
