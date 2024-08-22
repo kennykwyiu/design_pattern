82ebbcdfae581dcb325f1340f45c18d3d1ad458e
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-22 23:26:33 +0800

    composite - financial_application example

--- src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialApplication.java
+++ src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialApplication.java
@@ -2,14 +2,24 @@ package org.kenny.design.pattern.structural.composite.financial_application;
 
 public class FinancialApplication {
     public static void main(String[] args) {
-        Portfolio portfolio = new Portfolio("Investment Portfolio");
+        FinancialElement stock = new FinancialInstrument("AAPL", 1500.0);
+        FinancialElement bond = new FinancialInstrument("Government Bond", 2000.0);
 
-        FinancialElement stock = new FinancialInstrument("AAPL");
-        FinancialElement bond = new FinancialInstrument("Government Bond");
+        Portfolio portfolio1 = new Portfolio("Investment Portfolio");
+        portfolio1.addElement(stock);
+        portfolio1.addElement(bond);
 
-        portfolio.addElement(stock);
-        portfolio.addElement(bond);
+        FinancialElement gold = new FinancialInstrument("Gold ETF", 3000.0);
+        Portfolio portfolio2 = new Portfolio("Retirement Portfolio");
+        portfolio2.addElement(gold);
 
-        portfolio.display();
+        PortfolioGroup portfolioGroup = new PortfolioGroup("All Portfolios");
+        portfolioGroup.addPortfolio(portfolio1);
+        portfolioGroup.addPortfolio(portfolio2);
+
+        portfolioGroup.display();
+
+        double totalValue = portfolioGroup.calculateValue();
+        System.out.println("Total Value of All Portfolios: $" + totalValue);
     }
 }
