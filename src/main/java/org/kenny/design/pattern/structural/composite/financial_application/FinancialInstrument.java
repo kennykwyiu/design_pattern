5e9da61aa97524e89f1306bd554546a143d4f85d
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-23 22:29:56 +0800

    composite - financial_application example

--- src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialInstrument.java
+++ src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialInstrument.java
@@ -4,16 +4,18 @@ public class FinancialInstrument implements FinancialElement {
     private String name;
     private double value;
     private double performance;
+    private double volatility;
 
-    public FinancialInstrument(String name, double value) {
+    public FinancialInstrument(String name, double value, double volatility) {
         this.name = name;
         this.value = value;
         this.performance = 0.0;
+        this.volatility = volatility;
     }
 
     @Override
     public void display() {
-        System.out.println("Value: $" + value + " - Performance: " + performance);
+        System.out.println("Value: $" + value + " - Performance: " + performance + " - Volatility: " + volatility);
     }
 
     @Override
@@ -25,6 +27,11 @@ public class FinancialInstrument implements FinancialElement {
     public void trackPerformance(double price) {
         performance = ((price - value) / value) * 100;
         System.out.println("Tracking performance for " + name + ": " + performance + "%");
+    }
+
+    @Override
+    public double calculateRisk() {
+        return volatility;
     }
 
 }
