50742b42ab2c57f61035069bd786c84ee7108d25
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-23 22:24:35 +0800

    composite - financial_application example

--- src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialElement.java
+++ src/main/java/org/kenny/design/pattern/structural/composite/financial_application/FinancialElement.java
@@ -3,4 +3,5 @@ package org.kenny.design.pattern.structural.composite.financial_application;
 public interface FinancialElement {
     void display();
     double calculateValue();
+    void trackPerformance(double price);
 }
