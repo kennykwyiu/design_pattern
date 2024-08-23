15b4edcc1e1a47099159559341f63107e8dc9532
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-23 22:24:55 +0800

    composite - financial_application example

--- src/main/java/org/kenny/design/pattern/structural/composite/financial_application/Portfolio.java
+++ src/main/java/org/kenny/design/pattern/structural/composite/financial_application/Portfolio.java
@@ -1,35 +1,46 @@
 package org.kenny.design.pattern.structural.composite.financial_application;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Portfolio implements FinancialElement {
     private String name;
-    private List<FinancialElement> elements = new ArrayList<>();
+    private Map<String, FinancialElement> elements = new HashMap<>();
 
     public Portfolio(String name) {
         this.name = name;
     }
 
-    public void addElement(FinancialElement element) {
-        elements.add(element);
+    public void addElement(String symbol, FinancialElement element) {
+        elements.put(symbol, element);
     }
 
     @Override
     public void display() {
         System.out.println("Portfolio: " + name);
-        for (FinancialElement element : elements) {
-            element.display();
+        for (Map.Entry<String, FinancialElement> entry : elements.entrySet()) {
+            System.out.print("Symbol: " + entry.getKey() + " - ");
+            entry.getValue().display();
         }
     }
 
     @Override
     public double calculateValue() {
         double totalValue = 0;
-        for (FinancialElement element : elements) {
+        for (FinancialElement element : elements.values()) {
             totalValue += element.calculateValue();
         }
         return totalValue;
+    }
+
+    @Override
+    public void trackPerformance(double price) {
+        System.out.println("Tracking performance for Portfolio: " + name);
+        for (FinancialElement element : elements.values()) {
+            element.trackPerformance(price);
+        }
     }
 }
 
