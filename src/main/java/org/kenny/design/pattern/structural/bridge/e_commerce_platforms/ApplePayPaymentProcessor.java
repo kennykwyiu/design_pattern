4ec4875bf231d8552340440750f1a80827037fd6
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-27 20:34:23 +0800

    bridge - e_commerce_platforms example - ApplePayPaymentProcessor

---
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/ApplePayPaymentProcessor.java
@@ -0,0 +1,10 @@
+package org.kenny.design.pattern.structural.bridge.e_commerce_platforms;
+
+// New Concrete Implementor: ApplePayPaymentProcessor
+public class ApplePayPaymentProcessor implements PaymentProcessor {
+    @Override
+    public void processPayment() {
+        System.out.println("Processing payment using Apple Pay...");
+        // Apple Pay payment processing logic
+    }
+}
