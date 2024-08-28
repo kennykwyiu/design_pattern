c3c753dc9c6e57b194eaab699ea9b697c23b3786
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-28 20:31:03 +0800

    bridge - e_commerce_platforms example - PaymentGateway

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/PaymentGateway.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/PaymentGateway.java
@@ -2,10 +2,20 @@ package org.kenny.design.pattern.structural.bridge.e_commerce_platforms;
 
 abstract class PaymentGateway {
     protected PaymentProcessor paymentProcessor;
+    protected TransactionLogger transactionLogger;
 
-    public PaymentGateway(PaymentProcessor paymentProcessor) {
+    public PaymentGateway(PaymentProcessor paymentProcessor, TransactionLogger transactionLogger) {
         this.paymentProcessor = paymentProcessor;
+        this.transactionLogger = transactionLogger;
     }
 
-    abstract void processPayment();
+    void processPayment() {
+        try {
+            System.out.println("Initiating payment process...");
+            paymentProcessor.processPayment();
+            transactionLogger.logTransaction("Payment processed successfully.");
+        } catch (Exception e) {
+            transactionLogger.logTransaction("Payment processing failed. Error: " + e.getMessage());
+        }
+    }
 }
