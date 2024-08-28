fc707aa708f85b3bbc5c97b4b2c8532026cf1815
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-28 20:31:57 +0800

    bridge - e_commerce_platforms example - CreditCardPaymentGateway

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/CreditCardPaymentGateway.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/CreditCardPaymentGateway.java
@@ -2,12 +2,13 @@ package org.kenny.design.pattern.structural.bridge.e_commerce_platforms;
 
 public class CreditCardPaymentGateway extends PaymentGateway {
     public CreditCardPaymentGateway(PaymentProcessor paymentProcessor) {
-        super(paymentProcessor);
+        super(paymentProcessor, new ConsoleTransactionLogger());
     }
 
     @Override
     void processPayment() {
         System.out.println("Processing credit card payment...");
         paymentProcessor.processPayment();
+        transactionLogger.logTransaction(paymentProcessor.getClass().getSimpleName() + ": " + this.getClass().getSimpleName() + " processed successfully.");
     }
 }
