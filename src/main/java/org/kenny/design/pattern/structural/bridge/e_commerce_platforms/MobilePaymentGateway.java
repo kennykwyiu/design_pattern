e8526e1f63d5ff4b4865ce3d469e2b38ec4ef0d4
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-28 20:31:45 +0800

    bridge - e_commerce_platforms example - MobilePaymentGateway

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/MobilePaymentGateway.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/MobilePaymentGateway.java
@@ -1,14 +1,15 @@
 package org.kenny.design.pattern.structural.bridge.e_commerce_platforms;
 
 public class MobilePaymentGateway extends PaymentGateway {
-    public MobilePaymentGateway(PaymentProcessor paymentProcessor) {
-        super(paymentProcessor);
+    public MobilePaymentGateway(PaymentProcessor paymentProcessor,  TransactionLogger consoleLogger) {
+        super(paymentProcessor, consoleLogger);
     }
 
     @Override
     void processPayment() {
         System.out.println("Processing mobile payment...");
         paymentProcessor.processPayment();
+        transactionLogger.logTransaction(paymentProcessor.getClass().getSimpleName() + ": " + this.getClass().getSimpleName() + " processed successfully.");
     }
 }
 
