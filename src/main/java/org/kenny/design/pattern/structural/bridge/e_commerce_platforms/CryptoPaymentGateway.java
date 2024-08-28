ddbc93cc437a179fd45635ae35380dd2e6e143d0
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-28 20:31:31 +0800

    bridge - e_commerce_platforms example - CryptoPaymentGateway

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/CryptoPaymentGateway.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/CryptoPaymentGateway.java
@@ -1,13 +1,14 @@
 package org.kenny.design.pattern.structural.bridge.e_commerce_platforms;
 
 public class CryptoPaymentGateway extends PaymentGateway {
-    public CryptoPaymentGateway(PaymentProcessor paymentProcessor) {
-        super(paymentProcessor);
+    public CryptoPaymentGateway(PaymentProcessor paymentProcessor,  TransactionLogger consoleLogger) {
+        super(paymentProcessor, consoleLogger);
     }
 
     @Override
     void processPayment() {
         System.out.println("Processing cryptocurrency payment...");
         paymentProcessor.processPayment();
+        transactionLogger.logTransaction(paymentProcessor.getClass().getSimpleName() + ": " + this.getClass().getSimpleName() + " processed successfully.");
     }
 }
