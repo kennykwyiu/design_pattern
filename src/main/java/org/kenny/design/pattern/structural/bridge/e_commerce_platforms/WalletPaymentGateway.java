f424e2cab69d07336fa9231c392ec13e3b90dbf9
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-28 20:32:06 +0800

    bridge - e_commerce_platforms example - WalletPaymentGateway

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/WalletPaymentGateway.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/WalletPaymentGateway.java
@@ -1,13 +1,14 @@
 package org.kenny.design.pattern.structural.bridge.e_commerce_platforms;
 
 public class WalletPaymentGateway extends PaymentGateway {
-    public WalletPaymentGateway(PaymentProcessor paymentProcessor) {
-        super(paymentProcessor);
+    public WalletPaymentGateway(PaymentProcessor paymentProcessor,  TransactionLogger consoleLogger) {
+        super(paymentProcessor, consoleLogger);
     }
 
     @Override
     void processPayment() {
         System.out.println("Processing wallet payment...");
         paymentProcessor.processPayment();
+        transactionLogger.logTransaction(paymentProcessor.getClass().getSimpleName() + ": " + this.getClass().getSimpleName() + " processed successfully.");
     }
 }
