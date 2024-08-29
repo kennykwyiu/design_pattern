9be7060345115f065a2772ceb80c0a4544a9c648
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-29 21:49:58 +0800

    bridge - e_commerce_platforms example - ECommercePlatformExample

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/ECommercePlatformExample.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/ECommercePlatformExample.java
@@ -6,21 +6,25 @@ public class ECommercePlatformExample {
         PaymentProcessor stripeProcessor = new StripePaymentProcessor();
         TransactionLogger consoleLogger = new ConsoleTransactionLogger();
 
-        PaymentGateway creditCardGateway = new CreditCardPaymentGateway(paypalProcessor);
-        PaymentGateway walletGateway = new WalletPaymentGateway(stripeProcessor, consoleLogger);
+        Discount discount = new PercentageDiscount(10); // 10% discount
+        Reward reward = new PointReward(100); // 100 points reward
 
-        creditCardGateway.processPayment();
-        walletGateway.processPayment();
+        PaymentGateway creditCardGateway = new CreditCardPaymentGateway(paypalProcessor, consoleLogger, discount, reward);
+        PaymentGateway walletGateway = new WalletPaymentGateway(stripeProcessor, consoleLogger, discount, reward);
+
+        double paymentAmount = 100.0; // Example payment amount
+        creditCardGateway.processPayment(paymentAmount);
+        walletGateway.processPayment(paymentAmount);
 
 
         System.out.println("demonstrate the use of additional payment processors for Venmo and Apple Pay");
         PaymentProcessor venmoProcessor = new VenmoPaymentProcessor();
         PaymentProcessor applePayProcessor = new ApplePayPaymentProcessor();
 
-        PaymentGateway mobilePaymentGateway = new MobilePaymentGateway(venmoProcessor, consoleLogger);
-        PaymentGateway cryptoPaymentGateway = new CryptoPaymentGateway(applePayProcessor, consoleLogger);
+        PaymentGateway mobilePaymentGateway = new MobilePaymentGateway(venmoProcessor, consoleLogger, discount, reward);
+        PaymentGateway cryptoPaymentGateway = new CryptoPaymentGateway(applePayProcessor, consoleLogger, discount, reward);
 
-        mobilePaymentGateway.processPayment();
-        cryptoPaymentGateway.processPayment();
+        mobilePaymentGateway.processPayment(paymentAmount);
+        cryptoPaymentGateway.processPayment(paymentAmount);
     }
 }
