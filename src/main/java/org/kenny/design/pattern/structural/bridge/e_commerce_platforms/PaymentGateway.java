730c9f6d353aa2e59baaf4525b71ae0402d2b6ed
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-30 22:20:22 +0800

    bridge - e_commerce_platforms example - PaymentGateway

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/PaymentGateway.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/PaymentGateway.java
@@ -5,6 +5,7 @@ abstract class PaymentGateway {
     protected TransactionLogger transactionLogger;
     protected Discount discount;
     protected Reward reward;
+    protected PaymentCache paymentCache;
 
     public PaymentGateway(PaymentProcessor paymentProcessor, TransactionLogger transactionLogger, Discount discount, Reward reward) {
         this.paymentProcessor = paymentProcessor;
@@ -13,21 +14,34 @@ abstract class PaymentGateway {
         this.reward = reward;
     }
 
-    void processPayment(double amount) {
+    public void setPaymentCache(PaymentCache paymentCache) {
+        this.paymentCache = paymentCache;
+    }
+
+    void processPayment(String key, double amount) {
         try {
 //            System.out.println("Initiating payment process...");
 //            paymentProcessor.processPayment();
 //            transactionLogger.logTransaction("Payment processed successfully.");
 
-            System.out.println("Initiating payment process...");
-            double discountedAmount = discount.applyDiscount(amount);
-            System.out.println("After applying discount, the new amount is: " + discountedAmount);
+            double cachedAmount = paymentCache.getPayment(key);
+            if (cachedAmount == 0.0) {
+                System.out.println("Payment not found in cache. Processing payment...");
 
-            paymentProcessor.processPayment(discountedAmount);
+                System.out.println("Initiating payment process...");
+                double discountedAmount = discount.applyDiscount(amount);
+                System.out.println("After applying discount, the new amount is: " + discountedAmount);
+
+                paymentProcessor.processPayment(discountedAmount);
 //            transactionLogger.logTransaction("Payment processed successfully.");
-            transactionLogger.logTransaction(paymentProcessor.getClass().getSimpleName() + ": " + this.getClass().getSimpleName() + " processed successfully.");
+                transactionLogger.logTransaction(paymentProcessor.getClass().getSimpleName() + ": " + this.getClass().getSimpleName() + " processed successfully.");
 
-            reward.applyReward();
+                reward.applyReward();
+                paymentCache.cachePayment(key, discountedAmount);
+            } else {
+                System.out.println("Payment found in cache. Skipping payment processing.");
+                System.out.println("Cached payment amount: " + cachedAmount);
+            }
 
 
         } catch (Exception e) {
