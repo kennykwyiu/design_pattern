914ba4a6869c5b45eb8b537dac720740e29437a9
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-30 22:20:54 +0800

    bridge - e_commerce_platforms example - ECommercePlatformExample

--- src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/ECommercePlatformExample.java
+++ src/main/java/org/kenny/design/pattern/structural/bridge/e_commerce_platforms/ECommercePlatformExample.java
@@ -8,13 +8,17 @@ public class ECommercePlatformExample {
 
         Discount discount = new PercentageDiscount(10); // 10% discount
         Reward reward = new PointReward(100); // 100 points reward
+        PaymentCache paymentCache = new InMemoryPaymentCache();
 
         PaymentGateway creditCardGateway = new CreditCardPaymentGateway(paypalProcessor, consoleLogger, discount, reward);
         PaymentGateway walletGateway = new WalletPaymentGateway(stripeProcessor, consoleLogger, discount, reward);
+        creditCardGateway.setPaymentCache(paymentCache);
+        walletGateway.setPaymentCache(paymentCache);
 
+        String paymentKey = "user123_payment1";
         double paymentAmount = 100.0; // Example payment amount
-        creditCardGateway.processPayment(paymentAmount);
-        walletGateway.processPayment(paymentAmount);
+        creditCardGateway.processPayment(paymentKey, paymentAmount);
+        walletGateway.processPayment(paymentKey, paymentAmount);
 
 
         System.out.println("demonstrate the use of additional payment processors for Venmo and Apple Pay");
@@ -23,8 +27,10 @@ public class ECommercePlatformExample {
 
         PaymentGateway mobilePaymentGateway = new MobilePaymentGateway(venmoProcessor, consoleLogger, discount, reward);
         PaymentGateway cryptoPaymentGateway = new CryptoPaymentGateway(applePayProcessor, consoleLogger, discount, reward);
+        mobilePaymentGateway.setPaymentCache(paymentCache);
+        cryptoPaymentGateway.setPaymentCache(paymentCache);
 
-        mobilePaymentGateway.processPayment(paymentAmount);
-        cryptoPaymentGateway.processPayment(paymentAmount);
+        mobilePaymentGateway.processPayment(paymentKey, paymentAmount);
+        cryptoPaymentGateway.processPayment(paymentKey, paymentAmount);
     }
 }
