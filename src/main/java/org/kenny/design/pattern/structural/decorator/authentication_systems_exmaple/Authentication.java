51a807d8a8bb7a63c5fb9bbf9c98c5d39007a598
kennykwyiu <kenny.kwyiu@gmail.com>
2024-08-31 21:45:22 +0800

    decorator - authentication_systems_example - Authentication

---
+++ src/main/java/org/kenny/design/pattern/structural/decorator/authentication_systems_exmaple/Authentication.java
@@ -0,0 +1,5 @@
+package org.kenny.design.pattern.structural.decorator.authentication_systems_exmaple;
+
+public interface Authentication {
+    boolean authenticate(String username, String password);
+}
