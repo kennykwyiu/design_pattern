7486d8d40cb6fee342f9947535e273d940dab423
kennykwyiu <kenny.kwyiu@gmail.com>
2024-09-01 13:02:40 +0800

    decorator - authentication_systems_example - RoleBasedAccessControlDecorator

--- src/main/java/org/kenny/design/pattern/structural/decorator/authentication_systems_exmaple/RoleBasedAccessControlDecorator.java
+++ src/main/java/org/kenny/design/pattern/structural/decorator/authentication_systems_exmaple/RoleBasedAccessControlDecorator.java
@@ -1,8 +1,16 @@
 package org.kenny.design.pattern.structural.decorator.authentication_systems_exmaple;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RoleBasedAccessControlDecorator extends AuthenticationDecorator {
+    private Map<String, Role> roleMap;
+
+
+
     public RoleBasedAccessControlDecorator(Authentication authentication) {
         super(authentication);
+        roleMap = new HashMap<>();
     }
 
     @Override
@@ -22,5 +30,21 @@ public class RoleBasedAccessControlDecorator extends AuthenticationDecorator {
     private boolean checkUserRole(String username) {
         // Simulated role-based access control logic
         return "admin".equals(username); // Only allow access to users with the "admin" role
+    }
+
+    public void addRole(Role role) {
+        roleMap.put(role.getRoleName(), role);
+    }
+
+    public boolean checkAccess(User user, String resource) {
+        Role userRole = roleMap.get(user.getRole());
+
+        if (userRole != null) {
+            // Check if the user's role has access to the specified resource
+            // This is a simplified example, actual logic can be more complex
+            return userRole.getRoleName().equals("admin") && resource.equals("adminResource");
+        }
+
+        return false;
     }
 }
